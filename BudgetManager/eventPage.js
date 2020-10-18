83f5a3f6b280acd1d54b9ac48465f0bc7835d0bf
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-10-18 14:03:43 -0400

    update

--- BudgetManager/eventPage.js
+++ BudgetManager/eventPage.js
@@ -20,17 +20,22 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
                     newTotal += parseInt(budget.total);
                 }
                 newTotal += parseInt(clickData.selectionText);
-                if (newTotal >= budget.limit) {
-                    var notifOptions = {
-                        type: "basic",
-                        iconUrl: "icon48.png",
-                        title: "Limit reached!",
-                        message: "Uh oh, look's like you've reached your alloted limit."
-                    };
-                    chrome.notifications.create('limitNotif', notifOptions);
-                }
+                chrome.storage.sync.set({ 'total': newTotal }, function () {
+                    if (newTotal >= budget.limit) {
+                        var notifOptions = {
+                            type: "basic",
+                            iconUrl: "icon48.png",
+                            title: "Limit reached!",
+                            message: "Uh oh, look's like you've reached your alloted limit."
+                        };
+                        chrome.notifications.create('limitNotif', notifOptions);
+                    }
+                });
             });
-        });
         }
     }
+});
+
+chrome.storage.onChanged.addListener(function(changes,storageName){
+    chrome.browserAction.setBadgeText({"text":changes.total.newValue.toString()});
 });
