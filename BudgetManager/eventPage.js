675957f6864deda0f84f4928575c8b584108c9e6
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-10-17 20:09:01 -0400

    update

--- BudgetManager/eventPage.js
+++ BudgetManager/eventPage.js
@@ -1,12 +1,36 @@
-var contextMenuItem={
-    "id":"spendMoney",
-    "title":"SpendMoney",
-    "contexts":["selection"]
+var contextMenuItem = {
+    "id": "spendMoney",
+    "title": "SpendMoney",
+    "contexts": ["selection"]
 };
 chrome.contextMenus.create(contextMenuItem);
 
-function isInt(value){
-    return !isNaN(value)&&
-        parseInt(Number(value))==value&&
-        !isNaN(parseInt(value,10));
+function isInt(value) {
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
 }
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+        if (isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(['total', 'limit'], function (budget) {
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText);
+                if (newTotal >= budget.limit) {
+                    var notifOptions = {
+                        type: "basic",
+                        iconUrl: "icon48.png",
+                        title: "Limit reached!",
+                        message: "Uh oh, look's like you've reached your alloted limit."
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                }
+            });
+        });
+        }
+    }
+});
