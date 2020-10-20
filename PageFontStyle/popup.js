190f91abf4c141efd40fb302050dcb23ccb1a6f2
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-10-20 13:55:35 -0400

    update

--- PageFontStyle/popup.js
+++ PageFontStyle/popup.js
@@ -6,6 +6,6 @@ $(function () {
     $('#btnChange').click(function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { todo: "changeColor", clickedColor: color })
-        })
-    })
+        });
+    });
 });
