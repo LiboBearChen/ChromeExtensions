f2be23e4e3f0ea2dd5ed3230db3587b8b0b904d1
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-07 14:37:16 -0500

    update

--- PageAnalyse/eventPage.js
+++ PageAnalyse/eventPage.js
@@ -8,16 +8,28 @@ chrome.contextMenus.create(contextMenuItem);
 chrome.contextMenus.onClicked.addListener(function (clickData) {
     if (clickData.menuItemId == "saveWord" && clickData.selectionText) {
         chrome.storage.sync.get(['words'], function (storage) {
-            var newTotal = 0;
+            var newWords = [];
             if (storage.words) {
-                newTotal += parseInt(budget.total);
+                newWords=storage.words;
             }
-            newTotal += parseInt(clickData.selectionText);
-            chrome.storage.sync.set({ 'total': newTotal }, function () {});
+
+            //avoid duplicate words
+            var saved=false;
+            for(word in newWords){
+                if(word===clickData.selectionText){
+                    chrome.browserAction.setBadgeText({"text":"Already Saved"});
+                    saved=true;
+                }
+            }
+            if(!saved){
+                newWords.push(clickData.selectionText);
+                chrome.storage.sync.set({ 'words': newWords }, function () {});
+            }
+            
         });
     }
 });
 
 chrome.storage.onChanged.addListener(function(changes,storageName){
-    chrome.browserAction.setBadgeText({"text":changes.total.newValue.toString()});
+    chrome.browserAction.setBadgeText({"text":changes.words[words.length-1].toString()});
 });
