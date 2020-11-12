8b90cbba08fe5556b219d86dfca300809ef1d9aa
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-11 21:25:00 -0500

    update

--- PageAnalyse/popup.js
+++ PageAnalyse/popup.js
@@ -2,15 +2,16 @@
 $(function(){
 
     chrome.storage.sync.get(['words'],function(storage){
-        $('#total').text(storage.words);
-        $('#limit').text(budget.limit);
+        var wordLines = document.getElementById("wordLines");
+        ULTemplate(storage.words, wordLines);
     });
 
+    //build saved words in popup page
     function ULTemplate(items, element) {
         let resultsHTML = "";
     
         for (i = 0; i < items.length - 1; i++) {
-                resultsHTML += "<li>" + items[i] + <a > Remove </a> </li>";
+                resultsHTML += "<li>" + items[i] + "<a > Remove </a> </li>";
         }
     
         element.innerHTML = resultsHTML;
