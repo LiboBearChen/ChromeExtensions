e403ccb0e36b249a5069ca095ec2a2a0f3ddd918
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-13 15:26:14 -0500

    update

--- PageAnalyse/eventPage.js
+++ PageAnalyse/eventPage.js
@@ -25,11 +25,41 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
                 newWords.push(clickData.selectionText);
                 chrome.storage.sync.set({ 'words': newWords }, function () {});
             }
-            
         });
     }
 });
 
+//show badge on the icon
 chrome.storage.onChanged.addListener(function(changes,storageName){
     chrome.browserAction.setBadgeText({"text":changes.words[words.length-1].toString()});
+    performMark();
 });
+
+//mark words on pages
+function performMark() {
+    var keyword = document.getElementById("keyword").value;
+    var display = document.getElementById("fileContent");
+
+    var newcontent = "";
+
+    let spans = document.querySelectorAll('mark');
+
+    for (var i = 0; i < spans.length; i++) {
+        spans[i].outerHTML = spans[i].innerHTML;
+    }
+
+    var re = new RegExp(keyword, "gi");
+    var replaceText = "<mark id='markme'>$&</mark>";
+    var bookContent = display.innerHTML;
+
+    newcontent = bookContent.replace(re, replaceText);
+
+    display.innerHTML = newcontent;
+    var count = document.querySelectorAll('mark').length;
+    document.getElementById("searchstat").innerHTML = "found " + count + " matches";
+
+    if (count > 0) {
+        var element = document.getElementById("markme");
+        element.scrollIntoView();
+    }
+}
