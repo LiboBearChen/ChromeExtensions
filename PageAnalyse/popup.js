5030763697fdd236139b7fa9fff78bcefc9195ec
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-18 14:01:31 -0500

    update

--- PageAnalyse/popup.js
+++ PageAnalyse/popup.js
@@ -16,24 +16,31 @@ $(function () {
     function ULTemplate(items, element) {
         let resultsHTML = "";
         for (i = 0; i < items.length; i++) {
-            resultsHTML += "<li>" + items[i] + `<a href='javascript:removeWord(${i});' class='btn btn-outline-danger btn-sm float-right'> Remove </a> </li>`;
+            resultsHTML += "<li>" + items[i] + `<a id='${i}' class='btn btn-outline-danger btn-sm float-right'> Remove </a> </li>`;
         }
+        
         element.innerHTML = resultsHTML;
+
+        for (i = 0; i < items.length; i++) {
+            let wordAnchor = document.getElementById(i);
+            wordAnchor.index=i;
+        }
     }
 
     //remove one word
-    function removeWord (index) {
+    $('a').click(function (event) {
+        console.log(event)
         var newWords = [];
         chrome.storage.sync.get(['words'], function (storage) {
             newWords = storage.words;
         });
-        newWords.splice(index, 1);
+        newWords.splice(event.currentTarget.index, 1);
         chrome.storage.sync.set({ 'words': newWords }, function () { });
         refreshSavedWords(newWords);
-    }
+    });
 
     //clear all button clicked
-    $('#analyse').click(function () {
+    $('#clear').click(function () {
         var newWords = [];
         chrome.storage.sync.set({ 'words': newWords }, function () { });
         refreshSavedWords(newWords);
@@ -49,7 +56,7 @@ $(function () {
     });
 
     //actions when saved words change
-    chrome.storage.onChanged.addListener(function (changes, storage) {
+    /* chrome.storage.onChanged.addListener(function (changes, storage) {
 
         //show the last word by badge on the icon
         chrome.browserAction.setBadgeText({ "text": changes.words[words.length - 1].toString() });
@@ -58,7 +65,7 @@ $(function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { todo: "markWords", savedWords: storage.words })
         });
-    });
+    }); */
 
 });
 
@@ -68,3 +75,15 @@ function move(event){
     alert(event.data.param1);
     alert(event.data.param2);
 }
+
+$("p").on({
+    mouseover: function(){
+      $("body").css("background-color", "lightgray");
+    },  
+    mouseout: function(){
+      $("body").css("background-color", "lightblue");
+    }, 
+    click: function(){
+      $("body").css("background-color", "yellow");
+    }  
+  });
