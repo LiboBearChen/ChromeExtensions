6a76e841c7d046baca4764b597f4c3ad79ba9c99
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-19 15:51:08 -0500

    update

--- PageAnalyse/popup.js
+++ PageAnalyse/popup.js
@@ -8,7 +8,7 @@ $(function () {
 
     //refresh the saved words section on the popup page
     function refreshSavedWords(words) {
-        var wordLines = document.getElementById("wordLines");
+        let wordLines = document.getElementById("wordLines");
         ULTemplate(words, wordLines);
     }
 
@@ -16,32 +16,31 @@ $(function () {
     function ULTemplate(items, element) {
         let resultsHTML = "";
         for (i = 0; i < items.length; i++) {
-            resultsHTML += "<li>" + items[i] + `<a id='${i}' class='btn btn-outline-danger btn-sm float-right'> Remove </a> </li>`;
+            resultsHTML += "<li>" + items[i] + `<a id='${i}' class='btn btn-outline-danger btn-sm float-right'> Remove </a> </li><hr>`;
         }
-        
+
         element.innerHTML = resultsHTML;
 
         for (i = 0; i < items.length; i++) {
             let wordAnchor = document.getElementById(i);
-            wordAnchor.index=i;
+            wordAnchor.index = i;
         }
     }
 
     //remove one word
-    $('a').click(function (event) {
-        console.log(event)
-        var newWords = [];
+    $("ul").on("click", "a", function () {
+        let index = $(this).attr('id');
+        let newWords = [];
         chrome.storage.sync.get(['words'], function (storage) {
             newWords = storage.words;
+            newWords.splice(index, 1);
+            chrome.storage.sync.set({ 'words': newWords }, function () { });
         });
-        newWords.splice(event.currentTarget.index, 1);
-        chrome.storage.sync.set({ 'words': newWords }, function () { });
-        refreshSavedWords(newWords);
     });
 
     //clear all button clicked
     $('#clear').click(function () {
-        var newWords = [];
+        let newWords = [];
         chrome.storage.sync.set({ 'words': newWords }, function () { });
         refreshSavedWords(newWords);
     });
@@ -49,41 +48,43 @@ $(function () {
     //analyse button clicked
     $('#analyse').click(function () {
         chrome.storage.sync.get(['words'], function (storage) {
-            var words = storage.words;
+            let words = storage.words;
             let resultsHTML = "";
             $('#result').innerHTML = resultsHTML;
         });
     });
 
     //actions when saved words change
-    /* chrome.storage.onChanged.addListener(function (changes, storage) {
+    chrome.storage.onChanged.addListener(function (changes, storage) {
 
+        let wordLines = document.getElementById("wordLines");
+        ULTemplate(changes.words, wordLines);
         //show the last word by badge on the icon
-        chrome.browserAction.setBadgeText({ "text": changes.words[words.length - 1].toString() });
+        // chrome.browserAction.setBadgeText({ "text": changes.words[words.length - 1].toString() });
 
         //send message to content.js
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        /* chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { todo: "markWords", savedWords: storage.words })
-        });
-    }); */
+        }); */
+    });
 
 });
 
-$("selector").click({param1: "dwqd", param2: "wqdwq"}, move);
+$("selector").click({ param1: "dwqd", param2: "wqdwq" }, move);
 
-function move(event){
+function move(event) {
     alert(event.data.param1);
     alert(event.data.param2);
 }
 
 $("p").on({
-    mouseover: function(){
-      $("body").css("background-color", "lightgray");
-    },  
-    mouseout: function(){
-      $("body").css("background-color", "lightblue");
-    }, 
-    click: function(){
-      $("body").css("background-color", "yellow");
-    }  
-  });
+    mouseover: function () {
+        $("body").css("background-color", "lightgray");
+    },
+    mouseout: function () {
+        $("body").css("background-color", "lightblue");
+    },
+    click: function () {
+        $("body").css("background-color", "yellow");
+    }
+});
